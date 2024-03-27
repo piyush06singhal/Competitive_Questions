8f53d55f8fdc9d6c1ecedd62d00eb160840b8ee7
Piyush Singhal <164621184+piyush06singhal@users.noreply.github.com>
2024-03-27 22:15:34 +0530

    Add files via upload

---
+++ chef_coins_game.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n % 6 == 0)
+    {
+        cout << "Misha" << endl;
+    }
+    else
+    {
+        cout << "Chef" << endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
