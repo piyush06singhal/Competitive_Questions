adf295aac5af26e09f9c25b90e57c0056d060fa8
Piyush Singhal <164621184+piyush06singhal@users.noreply.github.com>
2024-05-03 20:31:51 +0530

    Add files via upload

---
+++ Alternate_work_Days.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, p, q;
+        cin >> a >> b >> p >> q;
+        int c = p / a;
+        int d = q / b;
+        if (abs(c - d) <= 1 && p % a == 0 && q % b == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
